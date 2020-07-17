1a9be81a4addc38f575d06e3d258090fb98c74d8
mariamfh9 <mfh.haider@gmail.com>
2020-07-17 16:40:36 -0400

    fixed application.rb

--- recipe-backend/config/application.rb
+++ recipe-backend/config/application.rb
@@ -21,10 +21,13 @@ Bundler.require(*Rails.groups)
 
 module RecipeBackend
   class Application < Rails::Application
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch]
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
