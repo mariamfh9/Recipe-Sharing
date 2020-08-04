637cb27f32a269fb0bb60030c581a497af2e7f7e
Mariam Haider <mfh.haider@gmail.com>
2020-08-04 00:13:10 -0400

    Update search.js

--- recipe-frontend/src/search.js
+++ recipe-frontend/src/search.js
@@ -1,17 +1,30 @@
-const searchForCategories = () => { // creates a function
-let searchBar = document.getElementById("searchBar"); // selects the element with id searchBar
+const displaySearch = () => {
+  let searchForm = document.getElementById("search-form");
+  let html = `
+  <form>
+    <label>Name</label>
+    <input type="text" id="name">
+    <input type="submit" value="Submit">
+  </form>
+  `;
 
-searchBar.addEventListener("keyup", (event) => { // on that searchBar element, adds an event listener that triggers every time a user lets go of a key
-  let categories = document.querySelectorAll("p"); // selects all the p elements (paragraphs) on the page
-  const search = event.target.value.toLowerCase(); // creates a string with the value of searchBar input element (and makes it all lowerCase)
-  Array.from(categories).forEach((category) => { // creates an array from all the p elements and loops over all of them
-    let categoryName = category.innerText; //gets the innerText of each p element
+  searchForm.innerHTML = html;
+  document.querySelector("form").addEventListener("submit", searchForCategories);
+}
 
-    if (categoryName.toLowerCase().indexOf(search) > -1) { // checks if the p element's text contains the same string value as the one in the search bar (true if it does, false if it doesn't)
-      category.parentElement.style.display = ""; // makes the element visible, by making the display property empty on the paragraph's parent if the text matches
-    } else {
-      category.parentElement.style.display = "none"; // hides the element if the text does not match the search text
+const searchForCategories = () => {
+  event.preventDefault();
+  console.log("Form clicked");
+
+  const category = {
+    name: document.getElementById("name").value,
+  };
+
+  fetch(CATEGORIES_URL)
+    .then((response) => response.json())  
+    .then((data) => {   
+    if (data == category.name){
+      
     }
   });
-});
-};
+}
