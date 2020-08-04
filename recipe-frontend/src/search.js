df27be4cf6a125753b42256ec0231a6fd0ce77cc
Mariam Haider <mfh.haider@gmail.com>
2020-08-04 00:28:56 -0400

    Update search.js

--- recipe-frontend/src/search.js
+++ recipe-frontend/src/search.js
@@ -10,20 +10,21 @@ const displaySearch = () => {
 
   searchForm.innerHTML = html;
   document.querySelector("form").addEventListener("submit", searchForCategories);
-}
+} //this works
+
+
+
 
 const searchForCategories = () => {
   event.preventDefault();
   console.log("Form clicked");
 
-  const category = {
-    name: document.getElementById("name").value,
-  };
+  input = document.getElementById("name"); 
 
   fetch(CATEGORIES_URL)
     .then((response) => response.json())  
     .then((data) => {   
-    if (data == category.name){
+    if (data == input){
       
     }
   });
